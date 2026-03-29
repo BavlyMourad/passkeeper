9629af18ba5d6a826c9837ab4ebcdc386288f894
BavlyMourad <bavleymourad@gmail.com>
2026-03-29 23:01:37 +0200

    feat: setup Riverpod & SharedPreferences

---
+++ lib/core/constants/shared_prefs_keys.dart
@@ -0,0 +1,5 @@
+class SharedPrefsKeys {
+  SharedPrefsKeys._();
+
+  static const language = 'LANGUAGE';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:passkeeper/app.dart';
+import 'package:passkeeper/core/providers/shared_prefs_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final sharedPreferences = await SharedPreferences.getInstance();
+
   runApp(
-    const PassKeeperApp(),
+    ProviderScope(
+      overrides: [sharedPrefsProvider.overrideWithValue(sharedPreferences)],
+      child: const PassKeeperApp(),
+    ),
   );
 }
