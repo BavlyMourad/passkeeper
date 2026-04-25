2db5cf69c82ed02ca3d8434df6e82e58a6693990
BavlyMourad <bavleymourad@gmail.com>
2026-04-25 19:04:05 +0300

    refactor: edit mobile & tablet scale points

--- lib/core/constants/size_config.dart
+++ lib/core/constants/size_config.dart
@@ -5,6 +5,6 @@ class SizeConfig {
   static const double tabletBreakPoint = 800.0;
 
   // Reference widths for font scaling math
-  static const double mobileScalePoint = 600.0;
-  static const double tabletScalePoint = 1000.0;
+  static const double mobileScalePoint = 360.0;
+  static const double tabletScalePoint = 744.0;
 }
