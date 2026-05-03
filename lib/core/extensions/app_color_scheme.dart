1608deb5ae55f8fa54d55156bc0494332149f86c
BavlyMourad <bavleymourad@gmail.com>
2026-05-03 23:30:40 +0300

    feat: add AppColorScheme extension for theme-aware colors

---
+++ lib/core/extensions/app_color_scheme.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:passkeeper/config/theme/app_colors.dart';
+
+extension AppColorScheme on ColorScheme {
+  bool get _isDarkTheme => brightness == Brightness.dark;
+
+  Color get textFieldBackground =>
+      _isDarkTheme ? AppColors.blueGreyDark : AppColors.greyLightest;
+
+  Color get buttonTextColor => AppColors.greyLightest;
+
+  Color get bottomSheetBackground =>
+      _isDarkTheme ? AppColors.blueGreyMedium : AppColors.blueLightest;
+
+  List<Color> get buttonGradient => _isDarkTheme
+      ? AppColors.darkButtonGradientColors
+      : AppColors.lightButtonGradientColors;
+
+  List<Color> get backgroundGradient => _isDarkTheme
+      ? AppColors.darkBackgroundGradientColors
+      : AppColors.lightBackgroundGradientColors;
+}
