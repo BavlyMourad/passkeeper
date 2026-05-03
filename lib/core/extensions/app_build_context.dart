6a6301140ee31cb4a0e8686943bc2a0ecb313501
BavlyMourad <bavleymourad@gmail.com>
2026-05-03 23:30:34 +0300

    refactor: optimize AppBuildContext to reuse screenWidth in isMobile

--- lib/core/extensions/app_build_context.dart
+++ lib/core/extensions/app_build_context.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:passkeeper/core/constants/size_config.dart';
 
 extension AppBuildContext on BuildContext {
   bool get isDarkTheme => Theme.of(this).brightness == Brightness.dark;
+
+  double get screenWidth => MediaQuery.sizeOf(this).width;
+
+  bool get isMobile => screenWidth <= SizeConfig.tabletBreakPoint;
 }
