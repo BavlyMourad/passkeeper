9c5adbb3db5638c4b6d4e1b655345f7c6cb761a5
BavlyMourad <bavleymourad@gmail.com>
2026-05-16 20:22:16 +0300

    feat: add secure storage repository

---
+++ lib/core/data/secure_storage_repository.dart
@@ -0,0 +1,34 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'secure_storage_repository.g.dart';
+
+@riverpod
+SecureStorageRepository secureStorageRepository(Ref ref) {
+  return SecureStorageRepository();
+}
+
+class SecureStorageRepository {
+  final _storage = const FlutterSecureStorage(
+    aOptions: AndroidOptions(),
+    iOptions: IOSOptions(
+      accessibility: KeychainAccessibility.first_unlock_this_device,
+    ),
+  );
+
+  Future<void> write(String key, String value) async {
+    await _storage.write(key: key, value: value);
+  }
+
+  Future<String?> read(String key) async {
+    return _storage.read(key: key);
+  }
+
+  Future<void> delete(String key) async {
+    await _storage.delete(key: key);
+  }
+
+  Future<void> deleteAll() async {
+    await _storage.deleteAll();
+  }
+}
