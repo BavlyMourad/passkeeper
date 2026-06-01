1b98df8efa37fbc603d2c7f09c32538188c36b5f
BavlyMourad <bavleymourad@gmail.com>
2026-06-01 22:07:06 +0300

    feat: add SearchAndFilterBar widget

---
+++ lib/features/passwords/presentation/widgets/search_and_filter_bar.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:passkeeper/config/l10n/app_localizations.dart';
+import 'package:passkeeper/config/theme/app_colors.dart';
+import 'package:passkeeper/core/constants/app_enums.dart';
+import 'package:passkeeper/core/constants/icon_paths.dart';
+import 'package:passkeeper/core/constants/size_config.dart';
+import 'package:passkeeper/core/extensions/app_build_context.dart';
+import 'package:passkeeper/core/widgets/app_icon.dart';
+import 'package:passkeeper/core/widgets/app_text_field.dart';
+
+class SearchAndFilterBar extends StatelessWidget {
+  const SearchAndFilterBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: SizeConfig.horizontalPadding(context.isMobile),
+      ),
+      child: Row(
+        spacing: 16.0,
+        children: [
+          Expanded(
+            child: AppTextField(
+              prefixIconPath: IconPaths.search,
+              hintText: AppLocalizations.of(context)!.search,
+            ),
+          ),
+          // TODO: Replace with a filter button
+          const AppIcon(
+            path: IconPaths.sort,
+            size: IconSize.medium,
+            color: AppColors.white,
+          ),
+        ],
+      ),
+    );
+  }
+}
