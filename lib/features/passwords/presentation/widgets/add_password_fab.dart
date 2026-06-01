bb8f794efe5105e2907ace7ee9c3e84ceda46728
BavlyMourad <bavleymourad@gmail.com>
2026-06-01 22:06:49 +0300

    feat: add AddPasswordFab widget

---
+++ lib/features/passwords/presentation/widgets/add_password_fab.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:passkeeper/config/theme/app_colors.dart';
+import 'package:passkeeper/core/constants/app_enums.dart';
+import 'package:passkeeper/core/constants/icon_paths.dart';
+import 'package:passkeeper/core/extensions/app_color_scheme.dart';
+import 'package:passkeeper/core/widgets/app_icon.dart';
+
+class AddPasswordFab extends StatelessWidget {
+  const AddPasswordFab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: () {},
+      backgroundColor: Colors.transparent,
+      elevation: 0.0,
+      highlightElevation: 0.0,
+      child: Ink(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: Theme.of(context).colorScheme.buttonGradient,
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+          ),
+          shape: BoxShape.circle,
+        ),
+        child: const Center(
+          child: AppIcon(
+            path: IconPaths.addPassword,
+            size: IconSize.medium,
+            color: AppColors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
