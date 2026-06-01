aa9960f13807e8bd0203ff0a19962c24c0e21837
BavlyMourad <bavleymourad@gmail.com>
2026-06-01 22:06:13 +0300

    chore: add dummy data constants for passwords and categories

---
+++ lib/core/constants/dummy_data.dart
@@ -0,0 +1,14 @@
+class Category {
+  final String id;
+  final String name;
+
+  Category({required this.id, required this.name});
+}
+
+List<Category> dummyCategories = [
+  Category(id: '1', name: 'Bank'),
+  Category(id: '2', name: 'Social Media'),
+  Category(id: '3', name: 'Games'),
+  Category(id: '4', name: 'Apps'),
+  Category(id: '5', name: 'Work'),
+];
