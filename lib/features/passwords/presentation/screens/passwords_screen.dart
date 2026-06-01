d36bd6180916c4aebc3f9ff2d1ce55d5abd9f6ca
BavlyMourad <bavleymourad@gmail.com>
2026-06-01 22:06:04 +0300

    feat: build PasswordsScreen with search, filter chips, and grid list

--- lib/features/passwords/presentation/screens/passwords_screen.dart
+++ lib/features/passwords/presentation/screens/passwords_screen.dart
@@ -1,11 +1,79 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:passkeeper/core/constants/app_strings.dart';
+import 'package:passkeeper/core/constants/image_paths.dart';
+import 'package:passkeeper/core/constants/size_config.dart';
+import 'package:passkeeper/core/extensions/app_build_context.dart';
+import 'package:passkeeper/core/styles/app_styles.dart';
+import 'package:passkeeper/core/widgets/app_image.dart';
+import 'package:passkeeper/core/widgets/gradient_background.dart';
+import 'package:passkeeper/core/widgets/language_selection_button.dart';
+import 'package:passkeeper/core/widgets/theme_toggle_button.dart';
+import 'package:passkeeper/features/passwords/presentation/widgets/add_password_fab.dart';
+import 'package:passkeeper/features/passwords/presentation/widgets/category_filter_chips.dart';
+import 'package:passkeeper/features/passwords/presentation/widgets/passwords_list.dart';
+import 'package:passkeeper/features/passwords/presentation/widgets/search_and_filter_bar.dart';
 
 class PasswordsScreen extends ConsumerWidget {
   const PasswordsScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const Scaffold(body: Center(child: Text('PASSWORDS')));
+    return GradientBackground(
+      begin: context.isDarkTheme ? Alignment.bottomRight : Alignment.centerLeft,
+      end: context.isDarkTheme ? Alignment.topLeft : Alignment.topRight,
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          scrolledUnderElevation: 0.0,
+          titleSpacing: 0.0,
+          title: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: SizeConfig.horizontalPadding(context.isMobile),
+            ),
+            child: Row(
+              spacing: 8.0,
+              children: [
+                AppImage(
+                  path: ImagePaths.logo,
+                  mobileSize: 32.0,
+                  tabletSize: 48.0,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+                Text(
+                  AppStrings.appName,
+                  style: AppStyles.titleLargeSemiBold(context),
+                ),
+              ],
+            ),
+          ),
+          actions: const [LanguageSelectionButton(), ThemeToggleButton()],
+        ),
+        body: const Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // No passwords yet
+            // TODO: handle later when we create controller
+            // const NoPasswordsYet(),
+            SizedBox(height: 24.0),
+
+            // Search and filter bar
+            SearchAndFilterBar(),
+
+            SizedBox(height: 24.0),
+
+            // Category filter chips
+            CategoryFilterChips(),
+
+            SizedBox(height: 40.0),
+
+            // Passwords list
+            Expanded(child: PasswordsList()),
+          ],
+        ),
+        floatingActionButton: const AddPasswordFab(),
+      ),
+    );
   }
 }
