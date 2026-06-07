ba9f9b362ea2886a132c5011c0079becc1780563
BavlyMourad <bavleymourad@gmail.com>
2026-06-07 23:16:15 +0300

    feat: wrap PasswordCard with InkWell for tap interaction

--- lib/features/passwords/presentation/widgets/password_card.dart
+++ lib/features/passwords/presentation/widgets/password_card.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:passkeeper/config/routes/app_routes.dart';
 import 'package:passkeeper/core/constants/app_enums.dart';
 import 'package:passkeeper/core/constants/icon_paths.dart';
 import 'package:passkeeper/core/styles/app_styles.dart';
@@ -10,45 +12,48 @@ class PasswordCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.all(0),
-      child: ListTile(
-        contentPadding: const EdgeInsets.symmetric(
-          horizontal: 12.0,
-          vertical: 0.0,
-        ),
-        leading: AppIcon(
-          path: IconPaths.password,
-          size: IconSize.medium,
-          color: Theme.of(context).colorScheme.onSecondary,
-        ),
-        title: Text('Facebook', style: AppStyles.titleSmallSemiBold(context)),
-        subtitle: Text(
-          'example@gmail.com',
-          style: AppStyles.captionRegular(
-            context,
-          ).copyWith(color: Theme.of(context).colorScheme.onSecondary),
-        ),
-        trailing: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            IconButton(
-              onPressed: () {},
-              icon: AppIcon(
-                path: IconPaths.favourite,
-                size: IconSize.small,
-                color: Theme.of(context).colorScheme.onSecondary,
+    return InkWell(
+      onTap: () => context.push(AppRoutes.passwordDetailsPath('1')),
+      child: Card(
+        margin: const EdgeInsets.all(0),
+        child: ListTile(
+          contentPadding: const EdgeInsets.symmetric(
+            horizontal: 12.0,
+            vertical: 0.0,
+          ),
+          leading: AppIcon(
+            path: IconPaths.password,
+            size: IconSize.medium,
+            color: Theme.of(context).colorScheme.onSecondary,
+          ),
+          title: Text('Facebook', style: AppStyles.titleSmallSemiBold(context)),
+          subtitle: Text(
+            'example@gmail.com',
+            style: AppStyles.captionRegular(
+              context,
+            ).copyWith(color: Theme.of(context).colorScheme.onSecondary),
+          ),
+          trailing: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: AppIcon(
+                  path: IconPaths.favourite,
+                  size: IconSize.small,
+                  color: Theme.of(context).colorScheme.onSecondary,
+                ),
               ),
-            ),
-            IconButton(
-              onPressed: () {},
-              icon: AppIcon(
-                path: IconPaths.copy,
-                size: IconSize.medium,
-                color: Theme.of(context).colorScheme.onSecondary,
+              IconButton(
+                onPressed: () {},
+                icon: AppIcon(
+                  path: IconPaths.copy,
+                  size: IconSize.medium,
+                  color: Theme.of(context).colorScheme.onSecondary,
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
