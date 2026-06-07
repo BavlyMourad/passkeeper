555284671219ae778fddcec9db0eb6338095a6c2
BavlyMourad <bavleymourad@gmail.com>
2026-06-07 23:15:58 +0300

    feat: register Password Details route

--- lib/config/routes/app_routes.dart
+++ lib/config/routes/app_routes.dart
@@ -2,6 +2,7 @@ import 'package:go_router/go_router.dart';
 import 'package:passkeeper/config/routes/app_shell.dart';
 import 'package:passkeeper/features/auth/presentation/screens/login_screen.dart';
 import 'package:passkeeper/features/passwords/presentation/screens/favourites_screen.dart';
+import 'package:passkeeper/features/passwords/presentation/screens/password_details_screen.dart';
 import 'package:passkeeper/features/passwords/presentation/screens/passwords_screen.dart';
 import 'package:passkeeper/features/splash/presentation/screens/splash_screen.dart';
 
@@ -11,6 +12,8 @@ class AppRoutes {
   static const splash = '/';
   static const login = '/login';
   static const passwords = '/passwords';
+  static const passwordDetails = '/passwords/:id'; // for GoRoute path
+  static String passwordDetailsPath(String id) => '/passwords/$id';
   static const favourites = '/favourites';
 
   // GoRouter configuration
@@ -24,6 +27,15 @@ class AppRoutes {
         builder: (context, state, child) => AppShell(child: child),
         routes: [
           GoRoute(path: passwords, builder: (_, __) => const PasswordsScreen()),
+
+          GoRoute(
+            path: passwordDetails,
+            builder: (context, state) {
+              final id = state.pathParameters['id']!;
+
+              return PasswordDetailsScreen(id: id);
+            },
+          ),
 
           GoRoute(
             path: favourites,
