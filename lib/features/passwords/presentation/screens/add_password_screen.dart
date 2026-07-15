843b8cfe6183059872f34a92d0b187ed0306b711
BavlyMourad <bavleymourad@gmail.com>
2026-07-15 21:43:02 +0300

    feat: convert to ConsumerWidget and wire submit to AddPasswordController

--- lib/features/passwords/presentation/screens/add_password_screen.dart
+++ lib/features/passwords/presentation/screens/add_password_screen.dart
@@ -1,16 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:passkeeper/config/l10n/app_localizations.dart';
 import 'package:passkeeper/core/constants/size_config.dart';
+import 'package:passkeeper/core/errors/app_exception.dart';
 import 'package:passkeeper/core/extensions/app_build_context.dart';
+import 'package:passkeeper/core/extensions/app_color_scheme.dart';
+import 'package:passkeeper/core/utils/app_utils.dart';
 import 'package:passkeeper/core/widgets/back_button.dart';
 import 'package:passkeeper/core/widgets/gradient_background.dart';
+import 'package:passkeeper/features/passwords/presentation/controllers/add_password_controller.dart';
 import 'package:passkeeper/features/passwords/presentation/widgets/password_form_header.dart';
 import 'package:passkeeper/features/passwords/presentation/widgets/password_form.dart';
 
-class AddPasswordScreen extends StatelessWidget {
+class AddPasswordScreen extends ConsumerWidget {
   const AddPasswordScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    ref.listen(addPasswordControllerProvider, (previous, next) {
+      // guard against the controller's default AsyncData(null) build state
+      // only proceed if we just transitioned out of loading
+      if (previous?.isLoading != true) return;
+
+      next.whenOrNull(
+        data: (_) {
+          context.pop();
+
+          AppUtils.showSnackBar(
+            context: context,
+            message: AppLocalizations.of(context)!.passwordSaved,
+            color: Theme.of(context).colorScheme.snackBar,
+          );
+        },
+        error: (error, _) {
+          final message = switch (error) {
+            SessionExpiredException() => AppLocalizations.of(
+              context,
+            )!.sessionExpired,
+            _ => AppLocalizations.of(context)!.genericError,
+          };
+
+          AppUtils.showSnackBar(context: context, message: message);
+        },
+      );
+    });
+
+    final isLoading = ref.watch(addPasswordControllerProvider).isLoading;
+
     return GradientBackground(
       begin: context.isDarkTheme ? Alignment.bottomRight : Alignment.centerLeft,
       end: context.isDarkTheme ? Alignment.topLeft : Alignment.topRight,
@@ -41,7 +78,14 @@ class AddPasswordScreen extends StatelessWidget {
 
                     const SizedBox(height: 50.0),
 
-                    PasswordForm(onSubmit: (password) {}),
+                    PasswordForm(
+                      isLoading: isLoading,
+                      onSubmit: (password) {
+                        ref
+                            .read(addPasswordControllerProvider.notifier)
+                            .submit(password);
+                      },
+                    ),
 
                     const SizedBox(height: 32.0),
                   ],
