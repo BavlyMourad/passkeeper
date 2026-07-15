1baca0762f2e3d6464d036cdb524f8ad754647c7
BavlyMourad <bavleymourad@gmail.com>
2026-07-15 21:38:15 +0300

    feat: add SessionExpiredException and PasswordNotFoundException

--- lib/core/errors/app_exception.dart
+++ lib/core/errors/app_exception.dart
@@ -5,3 +5,7 @@ class WrongPinException extends AppException {}
 class BiometricAuthException extends AppException {}
 
 class BiometricNotEnrolledException extends AppException {}
+
+class SessionExpiredException extends AppException {}
+
+class PasswordNotFoundException extends AppException {}
