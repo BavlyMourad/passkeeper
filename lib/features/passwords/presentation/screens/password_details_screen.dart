8dc254418c7059e0bf7b6f0ce39a7ac2666a195a
BavlyMourad <bavleymourad@gmail.com>
2026-07-21 22:23:12 +0300

    feat: load real password via PasswordDetailsController, wire edit/delete actions

--- lib/features/passwords/presentation/screens/password_details_screen.dart
+++ lib/features/passwords/presentation/screens/password_details_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:passkeeper/config/l10n/app_localizations.dart';
-import 'package:passkeeper/core/constants/dummy_data.dart';
 import 'package:passkeeper/core/constants/icon_paths.dart';
 import 'package:passkeeper/core/constants/size_config.dart';
 import 'package:passkeeper/core/extensions/app_build_context.dart';
@@ -10,6 +10,8 @@ import 'package:passkeeper/core/styles/app_styles.dart';
 import 'package:passkeeper/core/widgets/back_button.dart';
 import 'package:passkeeper/core/widgets/custom_button.dart';
 import 'package:passkeeper/core/widgets/gradient_background.dart';
+import 'package:passkeeper/core/widgets/loader.dart';
+import 'package:passkeeper/features/passwords/presentation/controllers/password_details_controller.dart';
 import 'package:passkeeper/features/passwords/presentation/widgets/edit_button.dart';
 import 'package:passkeeper/features/passwords/presentation/widgets/password_form.dart';
 import 'package:passkeeper/features/passwords/presentation/widgets/password_form_header.dart';
@@ -33,9 +35,8 @@ class _PasswordDetailsScreenState extends ConsumerState<PasswordDetailsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: CHANGE LATER
-    final password = dummyPasswords.firstWhere(
-      (password) => password.id == widget.id,
+    final passwordState = ref.watch(
+      passwordDetailsControllerProvider(widget.id),
     );
 
     return GradientBackground(
@@ -64,56 +65,82 @@ class _PasswordDetailsScreenState extends ConsumerState<PasswordDetailsScreen> {
             ),
           ],
         ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: SizeConfig.horizontalPadding(context.isMobile),
-            ),
-            child: Center(
-              child: SizedBox(
-                width: SizeConfig.tabletConstrainedWidth(
-                  context.screenWidth,
-                  context.isMobile,
+        body: passwordState.when(
+          loading: () => const Loader(),
+          error: (error, _) =>
+              Center(child: Text(AppLocalizations.of(context)!.genericError)),
+          data: (password) {
+            return SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: SizeConfig.horizontalPadding(context.isMobile),
                 ),
-                child: Column(
-                  children: [
-                    const SizedBox(height: 50.0),
-
-                    PasswordFormHeader(password: password),
-
-                    const SizedBox(height: 50.0),
-
-                    PasswordForm(
-                      password: password,
-                      onSubmit: (password) {
-                        _toggleEditMode();
-                      },
-                      isReadOnly: !_isEditingMode,
+                child: Center(
+                  child: SizedBox(
+                    width: SizeConfig.tabletConstrainedWidth(
+                      context.screenWidth,
+                      context.isMobile,
                     ),
+                    child: Column(
+                      children: [
+                        const SizedBox(height: 50.0),
 
-                    if (!_isEditingMode) ...[
-                      const SizedBox(height: 32.0),
-                      Row(
-                        spacing: 16.0,
-                        children: [
-                          Expanded(
-                            child: EditButton(onPressed: _toggleEditMode),
-                          ),
-                          Expanded(
-                            child: CustomButton(
-                              onPressed: () {},
-                              title: AppLocalizations.of(context)!.delete,
-                              prefixIconPath: IconPaths.delete,
-                            ),
+                        PasswordFormHeader(password: password),
+
+                        const SizedBox(height: 50.0),
+
+                        PasswordForm(
+                          password: password,
+                          isLoading: passwordState.isLoading,
+                          onSubmit: (editedPassword) {
+                            _toggleEditMode();
+
+                            ref
+                                .read(
+                                  passwordDetailsControllerProvider(
+                                    widget.id,
+                                  ).notifier,
+                                )
+                                .updatePassword(editedPassword);
+                          },
+                          isReadOnly: !_isEditingMode,
+                        ),
+
+                        if (!_isEditingMode) ...[
+                          const SizedBox(height: 32.0),
+                          Row(
+                            spacing: 16.0,
+                            children: [
+                              Expanded(
+                                child: EditButton(onPressed: _toggleEditMode),
+                              ),
+                              Expanded(
+                                child: CustomButton(
+                                  onPressed: () {
+                                    ref
+                                        .read(
+                                          passwordDetailsControllerProvider(
+                                            widget.id,
+                                          ).notifier,
+                                        )
+                                        .deletePassword();
+
+                                    context.pop();
+                                  },
+                                  title: AppLocalizations.of(context)!.delete,
+                                  prefixIconPath: IconPaths.delete,
+                                ),
+                              ),
+                            ],
                           ),
                         ],
-                      ),
-                    ],
-                  ],
+                      ],
+                    ),
+                  ),
                 ),
               ),
-            ),
-          ),
+            );
+          },
         ),
       ),
     );
