7a51515fa91c0585f975c3f05647a60f6f2ce70b
BavlyMourad <bavleymourad@gmail.com>
2026-07-21 22:23:57 +0300

    feat: load real favourites passwords and added Favourites headline

--- lib/features/passwords/presentation/screens/favourites_screen.dart
+++ lib/features/passwords/presentation/screens/favourites_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:passkeeper/config/l10n/app_localizations.dart';
 import 'package:passkeeper/core/constants/app_enums.dart';
 import 'package:passkeeper/core/constants/app_strings.dart';
 import 'package:passkeeper/core/constants/image_paths.dart';
@@ -10,7 +11,9 @@ import 'package:passkeeper/core/widgets/app_image.dart';
 import 'package:passkeeper/core/widgets/gradient_background.dart';
 import 'package:passkeeper/core/widgets/language_selection_button.dart';
 import 'package:passkeeper/core/widgets/theme_toggle_button.dart';
+import 'package:passkeeper/features/passwords/presentation/providers/passwords_provider.dart';
 import 'package:passkeeper/features/passwords/presentation/widgets/category_filter_chips.dart';
+import 'package:passkeeper/features/passwords/presentation/widgets/no_passwords_yet.dart';
 import 'package:passkeeper/features/passwords/presentation/widgets/passwords_list.dart';
 import 'package:passkeeper/features/passwords/presentation/widgets/search_and_filter_bar.dart';
 
@@ -19,6 +22,11 @@ class FavouritesScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final favouritePasswords = ref
+        .watch(passwordsProvider)
+        .where((password) => password.isFavourite)
+        .toList();
+
     return GradientBackground(
       begin: context.isDarkTheme ? Alignment.bottomRight : Alignment.centerLeft,
       end: context.isDarkTheme ? Alignment.topLeft : Alignment.topRight,
@@ -58,31 +66,46 @@ class FavouritesScreen extends ConsumerWidget {
             ),
           ],
         ),
-        body: const Column(
+        body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             // No passwords yet
-            // TODO: handle later when we create controller
-            // const NoPasswordsYet(),
-            SizedBox(height: 24.0),
+            if (favouritePasswords.isEmpty)
+              const NoPasswordsYet()
+            else ...[
+              const SizedBox(height: 50.0),
 
-            // Search and filter bar
-            SearchAndFilterBar(),
-
-            SizedBox(height: 24.0),
-
-            // Category filter chips
-            CategoryFilterChips(),
-
-            SizedBox(height: 40.0),
-
-            // Passwords list
-            Expanded(
-              child: PasswordsList(
-                isFavourite: true,
-                source: PasswordDetailsSource.favourites,
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: SizeConfig.horizontalPadding(context.isMobile),
+                ),
+                child: Text(
+                  AppLocalizations.of(context)!.favourites,
+                  style: AppStyles.headlineSemiBold(context),
+                ),
               ),
-            ),
+
+              const SizedBox(height: 40.0),
+
+              // Search and filter bar
+              const SearchAndFilterBar(),
+
+              const SizedBox(height: 24.0),
+
+              // Category filter chips
+              const CategoryFilterChips(),
+
+              const SizedBox(height: 40.0),
+
+              // Passwords list
+              Expanded(
+                child: PasswordsList(
+                  source: PasswordDetailsSource.favourites,
+                  passwords: favouritePasswords,
+                ),
+              ),
+            ],
           ],
         ),
       ),
